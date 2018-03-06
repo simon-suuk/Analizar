ef8e863793c26d22d03ee3a366ff64b464698df8
simon-suuk <simon.suuk@gmail.com>
2018-03-06 02:12:02 +0000

    merged register, login and knowledge_base functionalities. knowledge_base is yet to be tested

--- models/user_model.py
+++ models/user_model.py
@@ -1,15 +1,20 @@
 from models.base_model import *
-from peewee import CharField, BooleanField, TextField, DateTimeField
-
+from peewee import BooleanField, TextField, DateTimeField
+from flask_login import UserMixin
 from datetime import datetime
 
 
 class UserModel(BaseModel):
-    social_id = TextField(unique=True)
-    username = TextField()
-    email = TextField(null=True)
-    password = TextField()
-    is_active = BooleanField()
+    name = TextField(45)
+    email = TextField(45)
+    password_hash = TextField(255)
+    company_name = TextField(45)
+    industry = TextField(45)
+    company_size = TextField(45)
+    active = BooleanField(default=False)
     timestamp = DateTimeField(default=datetime.now)
-    page_id = TextField()
-    access_token = TextField()
+    social_id = TextField(unique=True, null=True)
+    social_username = TextField(null=True)
+    social_email = TextField(null=True)
+    page_id = TextField(null=True)
+    access_token = TextField(null=True)
